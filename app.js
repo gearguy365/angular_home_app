735226835d29c9999d1e840b58ffe551c6fe16e1
unknown <al.amin@Al-Amin.selise.com.bd>
2016-03-06 13:33:53 +0600

    6th march 1333

---
+++ app.js
@@ -0,0 +1,99 @@
+﻿angular.module('app', ['ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+        url: '/home',
+        templateUrl:'app/home/home.html'
+    })
+    .state('users', {
+        url: '/userList',
+        templateUrl:'app/users/user.list.html',
+        controller:'UsersController'
+    })
+    .state('users.userprofile', {
+        url: '/:id',
+        templateUrl:'app/users/user.profile.html',
+        controller:'UserAccountController'
+    });
+
+    $urlRouterProvider.otherwise('/home');
+})
+.controller('UsersController', function($scope,$state){
+    $scope.userdata = [
+          {
+             id:1,
+             firstname:"Scott",
+             lastname:"Jhon",
+             email:"scott@gmail.com"
+          },
+          {
+             id:2,
+             firstname:"Jhon",
+             lastname:"Doe",
+             email:"Jhon@gmail.com"
+          },
+          {
+             id:3,
+             firstname:"greg",
+             lastname:"papa",
+             email:"greg@gmail.com"
+          }];
+          $scope.goto = function(id) {
+              //the id here has to be same as the id in state
+              $state.go('users.userprofile', {id:id})
+          };
+})
+.controller('UserAccountController', function($scope,$stateParams,Data){
+    $scope.userdata = Data;
+    /*
+    $scope.userdata =[{
+             id:1,
+             firstname:"Scott",
+             lastname:"Jhon",
+             email:"scott@gmail.com"
+          },
+          {
+             id:2,
+             firstname:"Jhon",
+             lastname:"Doe",
+             email:"Jhon@gmail.com"
+          },
+          {
+             id:3,
+             firstname:"greg",
+             lastname:"papa",
+             email:"greg@gmail.com"
+          }];
+          */
+    $scope.userid = $stateParams.id;
+    $scope.getPerson = function () {
+        for (var i = 0; i < $scope.userdata.length; i++) {
+            if($scope.userdata[i].id == $scope.userid) {
+                return $scope.userdata[i];
+            }
+        }
+    }
+})
+
+.factory('Data', function() {
+    var userdata =
+        [{
+             id:1,
+             firstname:"Scott",
+             lastname:"Jhon",
+             email:"scott@gmail.com"
+          },
+          {
+             id:2,
+             firstname:"Jhon",
+             lastname:"Doe",
+             email:"Jhon@gmail.com"
+          },
+          {
+             id:3,
+             firstname:"greg",
+             lastname:"papa",
+             email:"greg@gmail.com"
+          }];
+          return userdata;
+});

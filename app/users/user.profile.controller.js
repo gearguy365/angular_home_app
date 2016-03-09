2525f7a11811a8a8302889d486e41822b1a4fd23
unknown <al.amin@Al-Amin.selise.com.bd>
2016-03-09 19:36:09 +0600

    9th march 1936

--- app/users/user.profile.controller.js
+++ app/users/user.profile.controller.js
@@ -41,16 +41,16 @@ angular.module('app')
 })
 .controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
 
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
+    $scope.items = items;
+    $scope.selected = {
+        item: $scope.items[0]
+    };
 
-  $scope.ok = function () {
-    $uibModalInstance.close($scope.selected.item);
-  };
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.selected.item);
+    };
 
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
 });
